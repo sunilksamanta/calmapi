d64a966ae006421a6cb2effce616a6e95fa3c258
Sunil Kumar Samanta <sunilthedj@gmail.com>
2021-03-29 16:12:58 +0530

    Logs updated

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ const {config} = require('./system/configs/config');
 
 const chalk = require('chalk');
 const packageInfo = require('./package.json');
-console.log(chalk.greenBright.bold('⬤⬤ Welcome to Calm API! v', packageInfo.version, '⬤⬤'));
+console.log(chalk.greenBright.bold.underline('⬤⬤ Welcome to Calm API! v', packageInfo.version, '⬤⬤'));
 
-
-server.listen(process.env.PORT || config.PORT).on('error', (err) => {
+const PORT = process.env.PORT || config.PORT;
+server.listen(PORT).on('error', (err) => {
     console.log(chalk.red('✘ Application failed to start'));
     console.error(chalk.red('✘', err.message));
     process.exit(0);
 }).on('listening', () => {
     console.log(chalk.greenBright('✔ Application Started'));
-    console.log(chalk.greenBright('✔ Keep Calm and REST'));
+    console.log(chalk.greenBright('✔ Listening on port', PORT));
+    console.log(chalk.greenBright.bold('✔ Keep Calm and REST'));
 });
