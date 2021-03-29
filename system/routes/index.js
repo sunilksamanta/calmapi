abb9f471ad18516506a80f57a94078c02c6dec7e
Sunil Kumar Samanta <sunilthedj@gmail.com>
2021-03-29 18:31:08 +0530

    Fix Consoles

--- system/routes/index.js
+++ system/routes/index.js
@@ -1,4 +1,12 @@
 'use strict';
 const router = require('express').Router;
+const chalk = require('chalk');
+
+// Define API Routes
+// Load Default Modules here
+console.log(chalk.greenBright('✔ Default Modules Loaded'));
+
+// Load user Defined Modules Here
+console.log(chalk.blueBright('✔ Custom Modules Loaded'));
 
 module.exports = {apiRoutes: router};
