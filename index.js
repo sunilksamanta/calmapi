f0b8be32b6a2a410fb11394ca6030a13270282e3
Sunil Kumar Samanta <sunilthedj@gmail.com>
2021-03-29 12:28:12 +0530

    Directory Structure Built

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 'use strict';
+const chalk = require('chalk');
+const packageInfo = require('./package.json');
+console.log(chalk.black.bold.bgBlue('** Welcome to Calm API! v', packageInfo.version));
+console.log(chalk.greenBright('Keep Calm and REST'));
