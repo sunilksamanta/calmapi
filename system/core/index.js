336b85863300f5fa66554b010a9904ed1db374fa
Sunil Kumar Samanta <sunilthedj@gmail.com>
2021-04-02 08:17:08 +0530

    Base Classes Directory renamed from classes to core

--- system/core/index.js
+++ system/core/index.js
@@ -3,10 +3,7 @@ const { CalmError } = require('./CalmError');
 const { CalmController } = require('./CalmController');
 const { CalmService } = require('./CalmService');
 const { CalmResponse } = require('./CalmResponse');
-/**
- * EXPORT Base Classes
- * @type {{CalmResponse: CalmResponse, CalmError: CalmError, CalmController: CalmController, CalmService: CalmService}}
- */
+
 module.exports = {
     CalmResponse,
     CalmError,
