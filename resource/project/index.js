98347ee759981587aa73b5a85f74323f29835c7e
Sunil Kumar Samanta <sunilthedj@gmail.com>
2021-04-03 12:20:28 +0530

    Base project moved to Resource/Project

---
+++ resource/project/index.js
@@ -0,0 +1,23 @@
+'use strict';
+// Load .env File
+require('dotenv').config();
+const chalk = require('chalk');
+const packageInfo = require('./package.json');
+console.log(chalk.greenBright.bold.underline('⬤⬤ Welcome to Calm API! v', packageInfo.version, '⬤⬤'));
+console.log(chalk.greenBright('✔ Application Started'));
+// Load Database
+require('./system/configs/database');
+
+// Load Server
+const { server } = require('./system/configs/server');
+const { config } = require('./system/configs/config');
+
+const PORT = process.env.PORT || config.PORT;
+server.listen(PORT).on('error', (err) => {
+    console.log(chalk.red('✘ Sorry!! We could not keep you calm because something just broke.'));
+    console.error(chalk.red('✘', err.message));
+    process.exit(0);
+}).on('listening', () => {
+    console.log(chalk.greenBright('✔ Listening on port', PORT));
+    console.log(chalk.greenBright.bold('✔ Keep Calm and REST'));
+});
