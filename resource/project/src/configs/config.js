c7d38d5787ed34fcf32a03cc4c2edc36456dc4da
Sunil Kumar Samanta <sunilthedj@gmail.com>
2021-04-03 14:58:22 +0530

    Config Override

---
+++ resource/project/src/configs/config.js
@@ -0,0 +1,9 @@
+'use strict';
+/**
+ * Custom Config File
+ * Override any existing config value in system/config/config.js
+ * @type {{}}
+ */
+module.exports.config = {
+
+};
--- resource/project/system/configs/config.js
+++ resource/project/system/configs/config.js
@@ -1,5 +1,33 @@
 'use strict';
-exports.config = {
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const config = {
     EXCLUDED_ITEMS_FROM_RESPONSE: [ 'password', '__v' ],
     PORT: 5001
+};
+const loadConfig = () => {
+    let finalConfig = { ...config };
+    try {
+        const userConfigPath = path.resolve(__dirname, '../../src/configs/config.js');
+        const userConfigExists = fs.existsSync(userConfigPath);
+        if(userConfigExists) {
+            const { config: userConfig } = require(userConfigPath);
+            if( typeof userConfig === 'object' && Object.keys(userConfig).length) {
+                finalConfig = { ...finalConfig, ...userConfig };
+                console.log(chalk.greenBright('✔ Custom Configurations Loaded'));
+            } else {
+                throw new Error('No Custom Configurations found');
+            }
+        }else {
+            throw new Error('No Custom Configurations found');
+        }
+    }catch (e) {
+        console.log(chalk.blueBright('✔', e.message, '* Using Defaults only'));
+    }
+    return finalConfig;
+};
+
+module.exports = {
+    config: loadConfig()
 };
