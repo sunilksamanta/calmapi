da828b7c2dc3651dee75d2724d8fe19f5fef71ac
Sunil Kumar Samanta <sunilthedj@gmail.com>
2021-04-17 20:17:56 +0530

    Mongodb Connection Added

--- resource/project/system/configs/database.js
+++ resource/project/system/configs/database.js
@@ -1,2 +1,35 @@
 'use strict';
+const mongoose = require( 'mongoose' );
+const { config } = require( './config' );
+const chalk = require('chalk');
 
+// Mongo Connection Class
+class Connection {
+    constructor() {
+        const url = config.MONGODB_URI;
+
+        mongoose.Promise = global.Promise;
+        mongoose.set( 'useNewUrlParser', true );
+        mongoose.set( 'useFindAndModify', false );
+        mongoose.set( 'useCreateIndex', true );
+        mongoose.set( 'useUnifiedTopology', true );
+        this.connect( url ).then( () => {
+            // eslint-disable-next-line no-console
+            console.log( chalk.greenBright('✔ Database Connected') );
+        } ).catch( ( err ) => {
+            // eslint-disable-next-line no-console
+            console.error( chalk.redBright(`✘ MONGODB ERROR: ${err.message}` ) );
+        } );
+
+    }
+
+    async connect( url ) {
+        try {
+            await mongoose.connect( url );
+        } catch ( e ) {
+            throw e;
+        }
+    }
+}
+
+module.exports = new Connection();
