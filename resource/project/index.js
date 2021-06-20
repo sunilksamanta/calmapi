961819c8d6543c350448ff105ee940f7e79052cb
Sunil <sunilthedj@gmail.com>
2021-06-20 12:35:25 +0530

    Welcome Text Change

--- resource/project/index.js
+++ resource/project/index.js
@@ -3,8 +3,9 @@
 require('dotenv').config();
 const chalk = require('chalk');
 const packageInfo = require('./package.json');
-console.log(chalk.greenBright.bold.underline('⬤  Welcome to Calm API! v', packageInfo.version, '⬤'));
-console.log(chalk.greenBright.bold('\n✔ Project: ', packageInfo.name));
+console.log(chalk.greenBright.bold('⬤  CALMAPI - Keep Calm and REST'));
+console.log(chalk.greenBright.bold('\n✔ Project: ', packageInfo.name, `v${ packageInfo.version}`));
+console.log(chalk.greenBright.bold('✔ CalmAPI Version: ', packageInfo.generatorVersion));
 console.log(chalk.greenBright('✔ Application Started'));
 // Load Database
 require('./system/configs/database');
@@ -15,11 +16,10 @@ const { config } = require('./system/configs/config');
 
 const PORT = process.env.PORT || config.PORT;
 server.listen(PORT).on('error', (err) => {
-    console.log(chalk.red('✘ Sorry!! We could not keep you calm because something just broke.'));
+    console.log(chalk.red('✘ Sorry!! Something just broke.'));
     console.error(chalk.red('✘', err.message));
     process.exit(0);
 }).on('listening', () => {
     console.log(chalk.greenBright('✔ Listening on port', PORT));
-    console.log(chalk.greenBright.bold('✔ Keep Calm and REST'));
     console.log(chalk.greenBright.bold(`✔ Endpoint Base URL: http://127.0.0.1:${PORT}`));
 });
